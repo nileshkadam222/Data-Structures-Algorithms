9e5c58caa23152efbd391d6d8f0a88bf3d909f50
Nilesh kadam <nilesh.kadam222@gmail.com>
2021-03-07 08:22:34 +0530

    test 1

---
+++ test_1/StackImplemetaion.java
@@ -0,0 +1,18 @@
+package com.datastructure.test_1;
+
+public class StackImplemetaion {
+
+	public static void main(String[] args) {
+		Stack s = new Stack(10);
+		s.push.accept(1);
+		s.push.accept(2);
+		s.push.accept(3);
+		s.push.accept(4);
+		s.push.accept(5);
+		s.printArray();
+		
+
+
+	}
+
+}
