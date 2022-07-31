338a4fee067ff354803f52eedcbd581dd9046d63
Nilesh kadam <nilesh.kadam222@gmail.com>
2022-07-31 11:55:55 +0530

    Hashtable Examples

---
+++ hashtable/FirstRecurringChar.java
@@ -0,0 +1,26 @@
+package com.datastructure.hashtable;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
+public class FirstRecurringChar {
+
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,5,2};
+        System.out.println(recurringFistChar(array));
+    }
+
+    private static Integer recurringFistChar(int[] array){
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<array.length;i++){
+            if(map.containsKey(array[i])){
+                return array[i];
+            }
+            map.put(array[i],0);
+        }
+        return null;
+    }
+
+
+}
