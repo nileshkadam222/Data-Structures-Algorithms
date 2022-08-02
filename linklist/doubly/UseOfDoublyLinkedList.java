2611bcc9cc46191f59c1360e3abbb4e5038f6986
Nilesh kadam <nilesh.kadam222@gmail.com>
2022-08-02 19:27:12 +0530

    Implementation of doubly linked list

---
+++ linklist/doubly/UseOfDoublyLinkedList.java
@@ -0,0 +1,26 @@
+package com.datastructure.linklist.doubly;
+
+public class UseOfDoublyLinkedList {
+
+    public static void main(String[] args) {
+        DoublyLinkedList l = new DoublyLinkedList();
+        l.append(10);
+        l.append(20);
+        l.append(30);
+        l.printListAsc();
+        l.printListDsc();
+        l.prepend(50);
+        l.printListAsc();
+        l.printListDsc();
+        l.insert(2,66);
+        l.insert(2,67);
+        l.insert(2,68);
+        l.printListAsc();
+        l.printListDsc();
+        l.delete(2);
+        l.printListAsc();
+        l.printListDsc();
+
+
+    }
+}
