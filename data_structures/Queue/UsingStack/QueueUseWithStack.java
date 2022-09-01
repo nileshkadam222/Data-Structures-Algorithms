fa4cfc902dd1ae7609e204782ecf657c65c49180
Nilesh kadam <nilesh.kadam222@gmail.com>
2022-09-01 19:51:49 +0530

    Queue implementation by using stack

---
+++ data_structures/Queue/UsingStack/QueueUseWithStack.java
@@ -0,0 +1,20 @@
+package com.datastructure.data_structures.Queue.UsingStack;
+
+import com.datastructure.data_structures.Queue.UseingNode.QueueStr;
+
+public class QueueUseWithStack {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		QueueUsingStack q = new QueueUsingStack();
+		q.push(1);
+		q.push(2);
+		q.push(3);
+		q.push(4);
+		q.display();
+		q.pop();
+		q.display();
+	}
+
+}
---
+++ data_structures/Queue/UsingStack/QueueUsingStack.java
@@ -0,0 +1,59 @@
+package com.datastructure.data_structures.Queue.UsingStack;
+
+import com.sun.org.apache.bcel.internal.classfile.InnerClass;
+
+import java.util.Stack;
+
+public class QueueUsingStack {
+
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+
+    private Integer front;
+
+    QueueUsingStack(){
+        this.stack1 = new Stack<>();
+        this.stack2 = new Stack<>();
+        this.front = 0;
+    }
+
+    public void push(Integer value){
+        if(this.stack1.isEmpty()){
+            this.front = value;
+        }
+
+        while (!this.stack1.isEmpty()){
+            this.stack2.push(this.stack1.pop());
+        }
+
+        this.stack1.push(value);
+
+        while (!this.stack2.isEmpty()){
+            this.stack1.push(this.stack2.pop());
+        }
+    }
+
+    public Integer pop(){
+        Integer res = stack1.pop();
+
+        if(!stack1.isEmpty()){
+            front = stack1.peek();
+        }
+        return res;
+    }
+
+    public boolean isEmpty(){
+        return stack1.isEmpty();
+    }
+
+    public Integer peek(){
+        return front;
+    }
+
+    public void display(){
+        System.out.println();
+        stack1.forEach(f->{
+            System.out.print(f+" -> ");
+        });
+    }
+}
